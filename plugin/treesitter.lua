b1712b43573a8a4990953f6e5bdec6f05be17408
lortkipa <nikusha191208@gmail.com>
2025-08-15 16:45:36 +0400

    installing treesitter for asm

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,10 +1,8 @@
 -- setup 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "html", "css", "javascript", "typescript", "lua", "vim", "json" },
+  ensure_installed = { "c", "cpp", "html", "css", "javascript", "typescript", "lua", "vim", "json", "asm" },
 
   auto_install = false,
-
-  ignore_install = { "javascript" },
 
   highlight = {
     enable = true,
